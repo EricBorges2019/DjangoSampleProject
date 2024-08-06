fd4b0afce901660dd7e55e8721a4dbfcf4227d0b
ericborges2019 <ericborges2019@fau.edu>
2024-08-06 11:02:17 -0400

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,8 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+
 
 
 class Question(models.Model):
@@ -8,7 +12,14 @@ class Question(models.Model):
     def __str__(self): #
             return self.question_text
 
+    def was_recently_published(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        #I don't like this solution
+        #feels too "clever" for its own good
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+        def __str__(self):
+            return self.choice_text
