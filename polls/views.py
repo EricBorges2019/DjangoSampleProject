2e4e0413f5faca750db582f4ba6732241e616370
ericborges2019 <ericborges2019@fau.edu>
2024-08-09 20:28:49 -0400

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 
+from .models import Question
+
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def details(request, question_id):
     response = "Results for Question %s:"
